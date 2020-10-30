347c906d4a3416c93db45df902dbb9d3b0f6e5b4
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-10-31 00:46:44 +0100

    Removed .gif from supported extensions

--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,4 @@
-SUPPORTED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
+SUPPORTED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png']
 
 YOLO_STRIDES = [8, 16, 32]
 YOLO_ANCHORS = [[[10,  13], [16,   30], [33,   23]],
